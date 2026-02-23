327c9f1eddc773a2384263229d654a01e5c426a3
ValVoy <v.cheron92@gmail.com>
2026-02-23 01:29:43 +0100

    Ajout des associations et des validations strictes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :attendances
+  has_many :events, through: :attendances
+  has_many :administered_events, foreign_key: 'admin_id', class_name: "Event"
 end
