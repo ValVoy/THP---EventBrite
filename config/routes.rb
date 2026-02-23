68d782ecc518e22ccecce6d063caf4cfb3599cfe
ValVoy <v.cheron92@gmail.com>
2026-02-23 16:51:43 +0100

    Création de la page profil avec restriction d'accès

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,18 @@
 Rails.application.routes.draw do
+  # Routes Devise générées automatiquement
   devise_for :users
-  get "home/index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
+  # Routes PWA
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
 
-  root "home#index"
+  # Routes RESTful pour les événements et les utilisateurs
+  resources :events
+  resources :users, only: [:show]
+
+  # La page d'accueil devient l'index des événements
+  root "events#index"
 end
