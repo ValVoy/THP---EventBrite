ee350ee69147e229bf388ea9864e8fce5fbfb8e6
ValVoy <v.cheron92@gmail.com>
2026-02-23 17:06:39 +0100

    Création de la page profil avec restriction d'accès

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,22 @@
 class UsersController < ApplicationController
+  # 1. Devise vérifie que la personne est connectée
+  before_action :authenticate_user!, only: [:show]
+  # 2. Notre méthode personnalisée vérifie que c'est bien le bon profil
+  before_action :is_current_user?, only: [:show]
+
   def show
+    @user = User.find(params[:id])
+    # On récupère tous les événements dont cet utilisateur est l'administrateur
+    @events = @user.administered_events
+  end
+
+  private
+
+  def is_current_user?
+    @user = User.find(params[:id])
+    unless current_user == @user
+      flash[:alert] = "Accès refusé : vous ne pouvez voir que votre propre profil."
+      redirect_to root_path
+    end
   end
 end
